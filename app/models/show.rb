0f4481c41e511a8ac81adf7a6940740b699cc51f
danielpdaniel <artofjquinn@gmail.com>
2022-12-19 18:49:28 -0500

    commit tree a72912f7b7b48b3fcb2350ac6cfd320849c72fa9

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    actors = []
+    self.characters.each do |character|
+        # actors << Actor.where(id: character.actor_id).full_name
+        actors << character.actor.full_name
+    end
+    actors
+  end
+
+  def sorry 
+    "We're sorry about passing on John Mulaney's pilot"
+  end
 end
